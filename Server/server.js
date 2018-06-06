b81ab5f4fc6004859d08909bbe8e132371cca926
ahmady016 <ahmady09@gmail.com>
2018-06-06 11:12:39 +0200

    setup Express Server

--- Server/server.js
+++ Server/server.js
@@ -1,1 +1,6 @@
-console.log("server running ...")
+import express from 'express';
+import env from './env';
+
+const PORT = process.env.PORT || env.PORT;
+const server = express();
+server.listen(PORT, () => console.log(`server running at port:${PORT} \n${new Date().toLocaleString()}`));
